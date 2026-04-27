8aeedb3cc9ddf260010f8702d39b91063765b531
khushi08928 <khushichawla105@gmail.com>
2026-04-27 15:56:03 +0530

    fix: update schema path in drizzle configuration to include src directory

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import {defineConfig} from "drizzle-kit";
 
 export default defineConfig({
-    schema: "./models/index.ts",
+    schema: "./src/models/index.ts",
     out: "./drizzle",
     dialect: "postgresql",
     dbCredentials: {
